76f7a1a828ad8da5ce7b79b9e3b24340dccf614e
Dario <dariomunao@gmail.com>
2025-02-12 01:54:33 +0100

    header section edited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
       return (
   
         <header id="header" className="header sticky-top">
-          <div className="topbar d-flex align-items-center">
+          {/* <div className="topbar d-flex align-items-center">
             <div className="container d-flex justify-content-center justify-content-md-between">
               <div className="contact-info d-flex align-items-center">
                 <i className="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
@@ -15,39 +15,20 @@ const Header = () => {
                 <a href="#" className="linkedin"><i className="bi bi-linkedin" /></a>
               </div>
             </div>
-          </div>{/* End Top Bar */}
+          </div> */}
           <div className="branding d-flex align-items-cente">
             <div className="container position-relative d-flex align-items-center justify-content-between">
-              <a href="index.html" className="logo d-flex align-items-center">
+              <a href="/" className="logo d-flex align-items-center">
                 {/* Uncomment the line below if you also wish to use an image logo */}
-                {/* <img src="assets/img/logo.png" alt=""> */}
-                <h1 className="sitename">BizLand</h1>
+                <img src="images/zuluLogo.png" alt=""/>
+                {/* <h1 className="sitename">Zulu Consulting</h1> */}
               </a>
               <nav id="navmenu" className="navmenu">
                 <ul>
-                  <li><a href="#hero" className="active">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#services">Services</a></li>
-                  <li><a href="#portfolio">Portfolio</a></li>
-                  <li><a href="#team">Team</a></li>
-                  <li className="dropdown"><a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown" /></a>
-                    <ul>
-                      <li><a href="#">Dropdown 1</a></li>
-                      <li className="dropdown"><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown" /></a>
-                        <ul>
-                          <li><a href="#">Deep Dropdown 1</a></li>
-                          <li><a href="#">Deep Dropdown 2</a></li>
-                          <li><a href="#">Deep Dropdown 3</a></li>
-                          <li><a href="#">Deep Dropdown 4</a></li>
-                          <li><a href="#">Deep Dropdown 5</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="#">Dropdown 2</a></li>
-                      <li><a href="#">Dropdown 3</a></li>
-                      <li><a href="#">Dropdown 4</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#contact">Contact</a></li>
+                  <li><a href="#hero" className="active">HOME</a></li>
+                  <li><a href="#about">ABOUT US</a></li>
+                  <li><a href="#services">SERVICES</a></li>
+                  <li><a href="#contact">CONTACT</a></li>
                 </ul>
                 <i className="mobile-nav-toggle d-xl-none bi bi-list" />
               </nav>
