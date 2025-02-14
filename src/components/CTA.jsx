6c1919b791f05ab94018818994af31908d2708ed
Dario <dariomunao@gmail.com>
2025-02-14 02:36:58 +0100

    cta section created

---
+++ src/components/CTA.jsx
@@ -0,0 +1,18 @@
+const CTA = () => {
+    return(
+        <section id="cta" className="cta section">
+            <div className="container" data-aos="fade-up" data-aos-delay={100}>
+                <div className="row cta-content skills-animation">
+                    <div className="col-lg-6 col-md-9 col-12 mx-auto my-5 text-center">
+                        <h3 className="mb-5">Trust us if you need professional services
+                        related to corporate security.</h3>
+                        <a className="cta-btn" href="/#contact">
+                            CONTACT US NOW!
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default CTA
