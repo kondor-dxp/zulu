3499c35cf74fc578e228e9d9d1abf4c2ad6c1f00
Dario <dariomunao@gmail.com>
2025-02-25 02:13:06 +0100

    projects section created

---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+const Projects = () => {
+    return(
+        <section id="projects" className="cta section">
+            <div className="container" data-aos="fade-up" data-aos-delay={100}>
+                <div className="row cta-content skills-animation">
+                    <div className="col-lg-6 col-md-9 col-12 mx-auto my-5 text-center">
+                        <h3 className="mb-5">Check our main projects</h3>
+                        <a className="cta-btn" href="https://risk-io.com/" target="_blank">
+                            Risk-io
+                        </a>
+                        <a className="cta-btn" href="https://www.nodevox.net/" target="_blank">
+                            NodeVox
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Projects
